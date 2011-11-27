b06061d41b857e318035e13061e635f7aa327112
Jorge Azevedo <jorge.amado.azevedo@gmail.com>
2011-11-27 01:16:48 +0000

    PID fixed
    Did not initialize return matrix. Bad

--- data/xenomailab/blocks/pid/pid.c
+++ data/xenomailab/blocks/pid/pid.c
@@ -1,3 +1,21 @@
+/*
+ * Xenomai Lab
+ * Copyright (C) 2011 Jorge Azevedo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
 #include "pid_settings.h"
@@ -35,7 +53,7 @@ double pid(double input, double Kp, double Td, double Ki)
 }
 
 Matrix periodic_function(Matrix* inputChannel,short numChannels){
-	Matrix ret;
+	Matrix ret=empty_matrix(1,1);
 
         ret.matrix[0][0]=pid(inputChannel[0].matrix[0][0],gs->kp,gs->td,gs->ki);
 
