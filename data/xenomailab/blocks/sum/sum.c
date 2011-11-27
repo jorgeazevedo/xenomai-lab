59edae604a86956682cee5529b51f885004b839a
Jorge Azevedo <jorge.amado.azevedo@gmail.com>
2011-11-27 01:15:42 +0000

    Sum fixed
    Stupid bug is fixed

--- data/xenomailab/blocks/sum/sum.c
+++ data/xenomailab/blocks/sum/sum.c
@@ -1,3 +1,21 @@
+/*
+ * Xenomai Lab
+ * Copyright (C) 2011 Jorge Azevedo
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 
 #include "sum_settings.h"
@@ -5,16 +23,23 @@
 Matrix periodic_function(Matrix* inputChannel,short numChannels){
 	Matrix ret=empty_matrix(1,1);
 	Matrix temp=empty_matrix(1,1);
+	int i=0;
 
 	temp=matrix_mul_double(&inputChannel[0], gs->Ch0Gain);
 	ret=matrix_sum(&ret,&temp);
 
-	temp=matrix_mul_double(&inputChannel[1], gs->Ch1Gain);
-	ret=matrix_sum(&ret,&temp);
-
-	if(numChannels>2){
-		temp=matrix_mul_double(&inputChannel[2], gs->Ch2Gain);
+	if(numChannels>=2){
+		temp=matrix_mul_double(&inputChannel[1], gs->Ch1Gain);
 		ret=matrix_sum(&ret,&temp);
+
+		if(numChannels>=3){
+			temp=matrix_mul_double(&inputChannel[2], gs->Ch2Gain);
+			ret=matrix_sum(&ret,&temp);
+
+			for(i=3;i<numChannels;i++){
+				ret=matrix_sum(&ret,&inputChannel[i]);
+			}
+		}
 	}
 
 	ret=matrix_mul_double(&ret,gs->OutputGain);
